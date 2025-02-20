78876b8c2c1f00bdaf9b901806b9c236dcb14626
moideen18 <158563065+moideen18@users.noreply.github.com>
2025-02-20 12:38:14 +0530

    task.js

--- backend/models/task.js
+++ backend/models/task.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
-
-const TaskSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  completed: { type: Boolean, default: false },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-});
-
-module.exports = mongoose.model("Task", TaskSchema);
+const taskSchema = new mongoose.Schema(
+  {
+    text: { type: String, required: true },
+    completed: { type: Boolean, default: false },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true } 
+);
+module.exports = mongoose.model("Task", taskSchema);
